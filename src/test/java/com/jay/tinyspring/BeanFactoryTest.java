f679844a1b8751c6c1397d5b1dfe0d02ac2f56a8
xuanjian <xuanjian@wacai.com>
2019-05-13 10:29:50 +0800

    add comment

--- src/test/java/com/jay/tinyspring/BeanFactoryTest.java
+++ src/test/java/com/jay/tinyspring/BeanFactoryTest.java
@@ -11,11 +11,14 @@ public class BeanFactoryTest {
 
     @Test
     public void test() {
+        // 1.初始化BeanFactory
         BeanFactory beanFactory = new BeanFactory();
 
+        // 2.注册BeanDefinition
         BeanDefinition beanDefinition = new BeanDefinition(new HelloWorldService());
         beanFactory.registerBeanDefinition("helloWorldService", beanDefinition);
 
+        // 3.获取Bean
         HelloWorldService helloWorldService = (HelloWorldService) beanFactory.getBean("helloWorldService");
         helloWorldService.sayHello();
     }
